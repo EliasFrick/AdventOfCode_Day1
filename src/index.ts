e8ef3262653c45480497e69a1c199f01f3b8ee8e
Elias <elias.frick.04@gmail.com>
2023-12-17 16:09:10 +0100

    Fertig

--- src/index.ts
+++ src/index.ts
@@ -3,58 +3,105 @@ import * as _ from 'lodash'
 
 export default function Main() {
 
-    const inputFileUrl = 'Input Tag 1.txt'
+    const inputFileUrl = 'Input.txt'
 
     const InputFile = fs.readFileSync(inputFileUrl,'utf8');
 
+    //const splittedLines = InputFile.split("\n")
     const splittedLines = InputFile.split("\n")
+    const splittedWords = InputFile.split(" ")
 
     const result: any[][]= [];
 
-    let endResult = 0;
+    let correctGame = true
 
-    let firstNumber =  false;
+    let sumGameIDs = 0
 
-    let numberPlace = 0;
+    let red = 0
+    let blue = 0
+    let green = 0
 
-    for (let i = 0; i < splittedLines.length; i++) {
-        result[i] = [];
-    }
-    
+    const GameID = 5
+    let twoGameIDs = false
+    let threeGameIDs = false
 
-    //x = Line
-    //y = Char
-    for (let x = 0; x < splittedLines.length; x++) {
-        for (let y = 0; y < splittedLines[x].length; y++) {
-            if (parseInt(splittedLines[x][y])) {
-                if (!result[x][numberPlace] && firstNumber === false) {
-                    //console.log(splittedLines[x][y])
-                    result[x][numberPlace] = splittedLines[x][y]
-                    numberPlace++;
-                    firstNumber = true
-                }
-                if (firstNumber) {
-                    //console.log(splittedLines[x][y])
-                    result[x][numberPlace] = splittedLines[x][y]
-                }
-                if (splittedLines[x].length === y) {
-                    result[x][1] = result[x][0]
-                }
+    let counter = 0
+
+    splittedLines.forEach((value) => {
+        let currentLine = ""
+        if (!twoGameIDs && !threeGameIDs) {
+            currentLine = splittedLines[Number(value[GameID]) - 1]
+        }
+        else if (twoGameIDs && !threeGameIDs) {
+            currentLine = splittedLines[Number("" + value[GameID] + value[GameID + 1]) - 1]
+        }
+        else if (threeGameIDs && !twoGameIDs) {
+            currentLine = splittedLines[Number("" + value[GameID] + value[GameID + 1] + value[GameID + 2]) - 1]
+        }
+        
+        const currentLineWords = currentLine.split(" ")
+        correctGame = true
+        counter++
+
+
+        for (let i = 0; i < currentLineWords.length; i++) {
+            if (currentLineWords[i].includes("red")){
+                red = red + Number(currentLineWords[i - 1])
+            }
+            if (currentLineWords[i].includes("blue")){
+                blue = blue + Number(currentLineWords[i - 1])
+            }
+            if (currentLineWords[i].includes("green")){
+                green = green + Number(currentLineWords[i - 1])
+            }
+
+            if (value[GameID] === "9") {
+                twoGameIDs = true
+            }
+            if ("" + value[GameID] + value[GameID + 1] === "99") {
+                twoGameIDs = false
+                threeGameIDs = true
+            }
+
+            if (red > 12 || green > 13 || blue > 14) {   
+                correctGame = false
+            }
+
+            //Reset if the string containts a ";"
+            if (currentLineWords[i].includes(";")) {
+                red = 0
+                blue = 0
+                green = 0
             }
         }
-        numberPlace = 0;
-        firstNumber = false
-    }
 
-    let calculator 
+        
 
-    //calculate result
-    for (let i = 0; i < splittedLines.length; i++) {
-        calculator = result[i][0].concat(result[i][1].toString())
-        endResult = endResult + Number(calculator)
-    }
+        if (correctGame) {
+            //const fullGameID = value[GameID].concat(value[GameID] + 1)
+            //console.log(fullGameID)
+            if (!twoGameIDs && !threeGameIDs) {
+                sumGameIDs = sumGameIDs + Number(value[GameID])
+            }
+            else if (twoGameIDs && !threeGameIDs) {
+                sumGameIDs = sumGameIDs + Number("" + value[GameID] + value[GameID + 1])
+            } else if (threeGameIDs && !twoGameIDs) {
+                sumGameIDs = sumGameIDs + Number("" + value[GameID] + value[GameID + 1] + value[GameID + 2])
+            }
+            
+            //sumGameIDs = sumGameIDs + Number(value[GameID])
+            red = 0
+            green = 0
+            blue = 0
+        }
+        else{
+            red = 0
+            green = 0
+            blue = 0
+        }
 
-    console.log(endResult)
+    })
+    console.log(sumGameIDs)
 }
 
 Main();
